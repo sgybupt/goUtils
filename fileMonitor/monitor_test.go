6baa3264690536026b85934a1da225523122108e
sgybupt <sgybupt@foxmail.com>
2021-07-01 20:46:24 +0800

    添加了一个高性能修复时停组件. 添加ftp 包装后的客户端

--- fileMonitor/monitor_test.go
+++ fileMonitor/monitor_test.go
@@ -10,7 +10,7 @@ func TestFull(t *testing.T) {
 	var monitor Monitor
 	_ = monitor.InitConfig(Config{
 		TickTime:      time.Millisecond * 50,
-		ToleranceTime: time.Millisecond * 500,
+		ToleranceTime: time.Millisecond * 200,
 		AimDir:        "/Users/su/tempDir",
 		DirLevel:      0, // 初始化的时候 添加的监听等级. 如果在监听文件夹底下继续创建文件夹, 那么无论层级都会加入监听
 	})
