5d758387d37604a63bc9d50b81ea9fac8fb3f8b4
sgybupt <sgybupt@foxmail.com>
2021-07-05 10:06:53 +0800

    add log

--- logger/struct.go
+++ logger/struct.go
@@ -4,10 +4,10 @@ type LogConfig struct {
 	LogPath string
 }
 
-func (l *LogConfig) InitConfig(config LogConfig) {
-	l.LogPath = config.LogPath
-}
-
-func (l *LogConfig) SetLogPath(p string) {
-	l.LogPath = p
-}
+//func (l *LogConfig) InitConfig(config LogConfig) {
+//	l.LogPath = config.LogPath
+//}
+//
+//func (l *LogConfig) SetLogPath(p string) {
+//	l.LogPath = p
+//}
